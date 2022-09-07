6b928af9a35ee272d39cc9832bf7a4b89503bf81
emad maleki <93859829+emadmaleki@users.noreply.github.com>
2022-09-07 16:18:05 +0430

    update server
    update server

--- server.js
+++ server.js
@@ -7,24 +7,6 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 const app = express();
 
-
-app.post('/uploadUser',(req,res) => {
-    if (req.files == null) {
-        return res.status(400).json({msg:"can't upload file!"});
-    } 
-    
-    const file = req.files.img;
-    file.mv(`${__dirname}/../public/images/users/${file.name}`,(err) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.json({fileName:file.name,filePath:`/images/users/${file.name}`})
-    });
-})  
-
-app.use(fileUpload());
 server.use(middlewares);
 server.use(router);
-
-app.listen(port);
 server.listen(port);
